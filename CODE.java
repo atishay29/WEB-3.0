4a104c8062150aebe0d8f7c373374a5703277a66
atishay29 <88703380+atishay29@users.noreply.github.com>
2022-05-21 10:06:40 +0530

    Update CODE.java

--- CODE.java
+++ CODE.java
@@ -1,37 +1,35 @@
-package loan;
-import java.util.*;
-public class loans {
-	String name;                                                      // USING PUBLIC CLASS loans FOR LOAN AND EMI CALCULATIONS
+public class record{String name;
 	float reqiured;
 	float interest;      //CREATING A JAVA CLASS
 	float duration;
 	float monthlyemi;
 	float totalloan;
-	loans(String s,float req,float inte,float dura){                 //JAVA CONSTRUCTOR
+	record(String s,float req,float inte,float dura){                 //JAVA CONSTRUCTOR
 		this.name=s;
 		this.reqiured=req;
-		this.interest=inte;                                           //DECLARING class loans elements.
+		this.interest=inte;
 		this.duration=dura;
 		this.totalloan=((req*inte)/1200)*dura+req;
 		this.monthlyemi=totalloan/dura;
 	System.out.print("Congrats! your loan have been approved, on an interest rate of 6.3% your monthly emi is "+monthlyemi+" and your total loan including interest is "+totalloan);
 	
 	}
+	}
 	
+
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Welcome to our loan services, we provide loans at interest rate of 6.3%");
 		System.out.println("Kindly enter your name");
-		String s=sc.nextLine();                                              // NAME OF USER
+		String s=sc.nextLine();                             //TAKING NAME OF APPLICANT
 		
-		System.out.println("Enter the required amount :");                    // TAKING INPUTS FROM USER.
-		float req=sc.nextFloat();                                              //AMOUNT REQUIRED BY USER
+		System.out.println("Enter the required amount :");
+		float req=sc.nextFloat();                           // TAKING REQUIRED AMOUNT
 		System.out.println("Kindly enter the number of months in which you would llike to pay the emi.");
-		float dura=sc.nextFloat();                                             // DURATION (IN MONTHS) FOR PAYMENTS 
-		float inte=6.3f;                                                      //DECLARING INTEREST RATE
-		loans approved=new loans(s,req,inte,dura);                     // CREATING AN INSTANCE OF CLASS USING JAVA CONSTRUCTOR FOR CLASS loans.
+		float dura=sc.nextFloat();                            //TAKING DURATION OF LOAN
+		float inte=6.3f;                                       //DECLARING INTEREST RATE
+		loans approved=new loans();                          // CREATING INSTANCE OF MAIN CLASS
+		approved.new record(s,req,inte,dura);              //  USING JAVA CONSTRUCTOR FOR CLASS record.
 		sc.close();
 
 	}
-
-}
